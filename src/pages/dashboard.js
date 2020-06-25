c9ee62cc980c834c106960e7a75038f85e832330
Joel <joelrzief@gmail.com>
2020-06-25 17:47:09 -0400

    Settup files and routes

---
+++ src/pages/dashboard.js
@@ -0,0 +1,31 @@
+import React, {useEffect} from 'react';
+import { Link, navigate } from 'gatsby';
+import { Router } from '@reach/router';
+import Layout from '../components/layout';
+import Profile from '../components/Profile';
+import RouteBase from '../components/route-base';  
+import RouteSecret from '../components/route-secret'; 
+import RouteLogin from '../components/route-login';
+
+
+
+const Dashboard = ({location}) => {
+  useEffect(() => {
+   if (location.pathname.match(/^\/dashboard\/?$/)) {
+     navigate('/dashboard/login', {replace: true})
+   }
+  }, []);
+  return (
+    <Layout>
+    <Profile />
+    <Router>
+      <RouteBase  path="/dashboard/base" />
+      <RouteSecret path="/dashboard/secret" />
+      <RouteLogin path="/dashboard/login" />
+    </Router>
+      <p>TODO: create a dashboard</p>
+    </Layout>
+  );
+};
+
+export default Dashboard;
